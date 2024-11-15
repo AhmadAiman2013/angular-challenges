cb4a2dd0e3cdc12fb40ab7376948096fe7b6ac13
Ahmad Aiman <ahmadaimanalias@gmail.com>
2024-11-15 17:12:09 +0800

    refactor(1-projection): changes card.component, list-item.component add: card-row.directives add: chnages to signal api for store

--- apps/angular/1-projection/src/app/component/teacher-card/teacher-card.component.ts
+++ apps/angular/1-projection/src/app/component/teacher-card/teacher-card.component.ts
@@ -1,8 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FakeHttpService } from '../../data-access/fake-http.service';
 import { TeacherStore } from '../../data-access/teacher.store';
-import { CardType } from '../../model/card.model';
-import { Teacher } from '../../model/teacher.model';
 import { CardComponent } from '../../ui/card/card.component';
 
 @Component({
@@ -24,17 +22,12 @@ import { CardComponent } from '../../ui/card/card.component';
   imports: [CardComponent],
 })
 export class TeacherCardComponent implements OnInit {
-  teachers: Teacher[] = [];
-  cardType = CardType.TEACHER;
+  private http = inject(FakeHttpService);
+  private store = inject(TeacherStore);
 
-  constructor(
-    private http: FakeHttpService,
-    private store: TeacherStore,
-  ) {}
+  teachers = this.store.teachers();
 
   ngOnInit(): void {
     this.http.fetchTeachers$.subscribe((t) => this.store.addAll(t));
-
-    this.store.teachers$.subscribe((t) => (this.teachers = t));
   }
 }
