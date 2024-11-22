2d38e79d2cfa4e05d5e18ef803c1e8272139717d
Ahmad Aiman <ahmadaimanalias@gmail.com>
2024-11-22 16:32:41 +0800

    refactor(injection): final changes

--- apps/angular/39-injection-token/src/app/data.ts
+++ apps/angular/39-injection-token/src/app/data.ts
@@ -1,1 +1,15 @@
+import { InjectionToken } from '@angular/core';
+
 export const DEFAULT_TIMER = 1000;
+
+export const TIMER_STATE = new InjectionToken<number>('TimerState', {
+  factory: () => DEFAULT_TIMER,
+});
+
+/**
+ * Provide a time value with type safety
+ * @param timer Timer value in ms
+ */
+export function provideTimer(timer: number) {
+  return { provide: TIMER_STATE, useValue: timer };
+}
