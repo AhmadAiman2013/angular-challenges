0fe1e414574d91e4922654529e5c73a69fc80de5
Ahmad Aiman <ahmadaimanalias@gmail.com>
2024-11-22 16:48:13 +0800

    refactor(pure-pipe): final changes

--- apps/angular/8-pure-pipe/src/app/app.component.ts
+++ apps/angular/8-pure-pipe/src/app/app.component.ts
@@ -1,21 +1,18 @@
-import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { ComputePipe } from './comput.pipe';
 
 @Component({
   standalone: true,
-  imports: [NgFor],
+  imports: [ComputePipe],
   selector: 'app-root',
   template: `
-    <div *ngFor="let person of persons; let index = index">
-      {{ heavyComputation(person, index) }}
-    </div>
+    @for (person of persons; track person) {
+      <div>
+        {{ person | compute: $index }}
+      </div>
+    }
   `,
 })
 export class AppComponent {
   persons = ['toto', 'jack'];
-
-  heavyComputation(name: string, index: number) {
-    // very heavy computation
-    return `${name} - ${index}`;
-  }
 }
