bb27c2445056f5770b2e9a45fd0f7ef53fa54bf1
Ahmad Aiman <ahmadaimanalias@gmail.com>
2024-11-22 12:21:36 +0800

    refactor(directive): first commit

--- apps/angular/6-structural-directive/src/app/user.store.ts
+++ apps/angular/6-structural-directive/src/app/user.store.ts
@@ -1,15 +1,31 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { User } from './user.model';
+import { computed, Injectable, signal } from '@angular/core';
+
+import { Role, User } from './user.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserStore {
-  private user = new BehaviorSubject<User | undefined>(undefined);
-  user$ = this.user.asObservable();
+  #user = signal<User | undefined>(undefined);
+
+  user = this.#user.asReadonly();
+
+  isAdmin = computed(() => !!this.user()?.isAdmin);
 
   add(user: User) {
-    this.user.next(user);
+    this.#user.set(user);
+  }
+
+  matchesRole(roles: Role[]) {
+    const user = this.user();
+    const userRoles = user?.roles ?? [];
+    return roles.some((role) => userRoles.includes(role));
+  }
+
+  matches(matchRoles: Role[] | Role) {
+    return (
+      this.isAdmin() ||
+      this.matchesRole(Array.isArray(matchRoles) ? matchRoles : [matchRoles])
+    );
   }
 }
