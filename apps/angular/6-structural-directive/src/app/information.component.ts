401029ac7c85f605f0e4f6d0e9df85fc4a998fce
Ahmad Aiman <ahmadaimanalias@gmail.com>
2024-11-22 14:49:21 +0800

    refactor(directive): final changes

--- apps/angular/6-structural-directive/src/app/information.component.ts
+++ apps/angular/6-structural-directive/src/app/information.component.ts
@@ -9,15 +9,11 @@ import { HasRoleDirective } from './has-role.directive';
   template: `
     <h2 class="mt-10 text-xl">Information Panel</h2>
     <!-- admin can see everything -->
-    <div [hasRoleSuperAdmin]="true">visible only for super admin</div>
-    <div [hasRole]="'MANAGER'">visible if manager</div>
-    <div [hasRole]="['MANAGER', 'READER']">
-      visible if manager and/or reader
-    </div>
-    <div [hasRole]="['MANAGER', 'WRITER']">
-      visible if manager and/or writer
-    </div>
-    <div [hasRole]="'CLIENT'">visible if client</div>
+    <div *hasRoleSuperAdmin="true">visible only for super admin</div>
+    <div *hasRole="'MANAGER'">visible if manager</div>
+    <div *hasRole="['MANAGER', 'READER']">visible if manager and/or reader</div>
+    <div *hasRole="['MANAGER', 'WRITER']">visible if manager and/or writer</div>
+    <div *hasRole="'CLIENT'">visible if client</div>
     <div>visible for everyone</div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
