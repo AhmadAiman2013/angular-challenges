5e9df75170ea89ee01953f7cc89689d1f0a03502
Ahmad Aiman <ahmadaimanalias@gmail.com>
2024-12-02 15:50:36 +0800

    refactor(default-onpush): final changes

--- apps/performance/34-default-vs-onpush/src/app/person-list.component.ts
+++ apps/performance/34-default-vs-onpush/src/app/person-list.component.ts
@@ -1,68 +1,54 @@
-import { Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  model,
+} from '@angular/core';
 
 import { CDFlashingDirective } from '@angular-challenges/shared/directives';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
+import { TitleCasePipe } from '@angular/common';
 import { MatListModule } from '@angular/material/list';
+import { AddPersonComponent } from './add-person.component';
+import { PersonRowComponent } from './person-row.component';
 
 @Component({
   selector: 'app-person-list',
   standalone: true,
   imports: [
-    CommonModule,
-    FormsModule,
+    TitleCasePipe,
     MatListModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatChipsModule,
     CDFlashingDirective,
+    AddPersonComponent,
+    PersonRowComponent,
   ],
   template: `
     <h1 cd-flash class="text-center font-semibold" title="Title">
-      {{ title | titlecase }}
+      {{ title() | titlecase }}
     </h1>
 
-    <mat-form-field class="w-4/5" cd-flash>
-      <input
-        placeholder="Add one member to the list"
-        matInput
-        type="text"
-        [(ngModel)]="label"
-        (keydown)="handleKey($event)" />
-    </mat-form-field>
+    <app-add-person (addName)="addName($event)" />
 
     <mat-list class="flex w-full">
-      <div *ngIf="names?.length === 0" class="empty-list-label">Empty list</div>
-      <mat-list-item
-        *ngFor="let name of names"
-        cd-flash
-        class="text-orange-500">
-        <div MatListItemLine class="flex justify-between">
-          <h3 title="Name">
-            {{ name }}
-          </h3>
-        </div>
-      </mat-list-item>
-      <mat-divider *ngIf="names?.length !== 0"></mat-divider>
+      @for (name of names(); track name) {
+        <app-person-row [name]="name" />
+      } @empty {
+        <div class="empty-list-label">Empty list</div>
+      }
+      @if (names().length !== 0) {
+        <mat-divider />
+      }
     </mat-list>
   `,
   host: {
     class: 'w-full flex flex-col items-center',
   },
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PersonListComponent {
-  @Input() names: string[] = [];
-  @Input() title = '';
+  names = model.required<string[]>();
+  title = input.required<string>();
 
-  label = '';
-
-  handleKey(event: KeyboardEvent) {
-    if (event.key === 'Enter') {
-      this.names?.unshift(this.label);
-      this.label = '';
-    }
+  addName(name: string) {
+    this.names.update((names) => [...names, name]);
   }
 }
