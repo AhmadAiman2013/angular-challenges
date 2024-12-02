96f08fd3ca2211f9d4d9fa60fdd2e51bb1794490
Ahmad Aiman <ahmadaimanalias@gmail.com>
2024-12-02 11:17:36 +0800

    refactor(catch-error): final changes

--- apps/rxjs/38-catch-error/src/app/app.component.ts
+++ apps/rxjs/38-catch-error/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, DestroyRef, OnInit, inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormsModule } from '@angular/forms';
+import { tapResponse } from '@ngrx/operators';
 import { Subject, concatMap, map } from 'rxjs';
 
 @Component({
@@ -42,20 +43,24 @@ export class AppComponent implements OnInit {
       .pipe(
         map(() => this.input),
         concatMap((value) =>
-          this.http.get(`https://jsonplaceholder.typicode.com/${value}/1`),
+          this.http.get(`https://jsonplaceholder.typicode.com/${value}/1`).pipe(
+            tapResponse({
+              next: (value) => {
+                console.log(value);
+                this.response = value;
+              },
+              error: (error) => {
+                console.log(error);
+                this.response = error;
+              },
+              complete: () => {
+                console.log('done');
+              },
+            }),
+          ),
         ),
         takeUntilDestroyed(this.destroyRef),
       )
-      .subscribe({
-        next: (value) => {
-          console.log(value);
-          this.response = value;
-        },
-        error: (error) => {
-          console.log(error);
-          this.response = error;
-        },
-        complete: () => console.log('done'),
-      });
+      .subscribe();
   }
 }
